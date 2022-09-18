578526d9be0310eecb03256cfd50541a8fee9514
gastyftacla <99477542+gastyft@users.noreply.github.com>
2022-09-18 16:41:31 -0300

    Add files via upload

---
+++ src/main/java/com/miportfolio/dto/MensajeCloudinary.java
@@ -0,0 +1,19 @@
+
+package com.miportfolio.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MensajeCloudinary {
+       
+    private String mensajeCloudinary;
+
+    public MensajeCloudinary() {
+    }
+
+    public MensajeCloudinary(String mensajeColudinary) {
+        this.mensajeCloudinary = mensajeCloudinary;
+    }
+
+}
